73d2cb1db7236a0a94e2f27f462826dcd5095704
StanEffy <holydonk@gmail.com>
2025-04-25 08:27:46 +0300

    api

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,5 +1,5 @@
 // API Configuration
-export const API_BASE_URL = 'http://localhost:8080';
+export const API_BASE_URL = 'http://aixx5ss5rf.loclx.io';
 
 // Auth Endpoints
 export const AUTH_ENDPOINTS = {
